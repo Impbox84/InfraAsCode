9db6dc876c2e5baaa58c99145b23cfacc9e4c056
sch.bry <bschoellkopf@cerp-rrm.com>
2026-05-22 17:25:42 +0200

    Finalization of the CI/CD Pipeline with GitHub Actions and test

--- terraform/bootstrap/backend/main.tf
+++ terraform/bootstrap/backend/main.tf
@@ -12,3 +12,36 @@ resource "aws_s3_bucket_versioning" "states_bucket_versioning" {
         status = "Enabled"
     }
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+    url = "https://token.actions.githubusercontent.com"
+    client_id_list = ["sts.amazonaws.com"]
+    thumbprint_list = ["d89e3bd43d5d909b47a18977aa9d5ce36cee184c"]
+}
+
+resource "aws_iam_role" "github_actions_role" {
+    name = "GitHubActionsRole"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
+            Action = "sts:AssumeRoleWithWebIdentity"
+            Condition = {
+                StringLike = {
+                    "token.actions.githubusercontent.com:sub": "repo:Impbox84/InfraAsCode:*"
+                }
+            }
+        }]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+    role = aws_iam_role.github_actions_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_role_arn" {
+    value = aws_iam_role.github_actions_role.arn
+}
